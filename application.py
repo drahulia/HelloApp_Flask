2b72bd0081a0a0f48c0c656a969f075147a194cb
drahulia <bskxk82h@gmail.com>
2021-04-12 16:50:06 +0900

    first commit

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  return 'Hello World!'
+
+@app.route('/profile/<string:user_name>')
+def profile(user_name):
+  return 'displaying profile of {name}'.format(name=user_name)
+
+
+if __name__ == '__main__':
+    app.run()
